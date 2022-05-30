159b442357f78a6f9dc18863dd4b5f944ec9916e
VivoBook <kutu>
2022-05-30 18:48:11 +0600

    commit k

--- src/main/java/azmat/secondprojectcompany/SecondProjectCompanyApplication.java
+++ src/main/java/azmat/secondprojectcompany/SecondProjectCompanyApplication.java
@@ -9,6 +9,8 @@ public class SecondProjectCompanyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SecondProjectCompanyApplication.class, args);
         System.out.println("Hello world");
+
+        System.out.println("Aza bratan");
     }
 
 }
