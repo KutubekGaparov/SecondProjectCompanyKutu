81826b425e7cb96dfe0e0e864439a556ad2f5fe5
= <=>
2022-06-01 19:12:42 +0300

    2-Comit Azamat

---
+++ src/main/java/azmat/secondprojectcompany/model/din/ReligionSchool.java
@@ -0,0 +1,29 @@
+package azmat.secondprojectcompany.model.din;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "religion_school")
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class ReligionSchool {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "hibernate_gen")
+    @SequenceGenerator(name = "hibernate_gen", sequenceName = "hibernate_seq", allocationSize = 1)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String schoolName;
+    private String director;
+    private LocalDate directorBirthDay;
+    private String address;
+    private int phone;
+    private String email;
+}
