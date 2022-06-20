20803195f25f27a2811a170c3ab137379a9a64a1
VivoBook <kutu>
2022-06-20 20:05:01 +0600

    aws

--- src/main/java/azmat/secondprojectcompany/SecondProjectCompanyApplication.java
+++ src/main/java/azmat/secondprojectcompany/SecondProjectCompanyApplication.java
@@ -10,6 +10,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
@@ -28,6 +29,10 @@ public class SecondProjectCompanyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SecondProjectCompanyApplication.class, args);
         System.out.println("Hello world");
+    }
+    @GetMapping("/")
+    public String greetingPage(){
+        return "<h1>Welcome to Mady application!!!<h1/>";
     }
 
     @PostConstruct
